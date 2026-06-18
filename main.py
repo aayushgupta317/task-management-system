797cfe5fd4057d74d2a9715cae07e1b4ab1d6429
Draco <thatdracoyt@gmail.com>
2026-06-18 20:18:44 +0500

    Does nothing at all

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ def menu():
 	print("1. Add Task")
 	print("2. View Tasks")
 	print("3. Complete Tasks")
+	print("4. Do nothing ")
 
-menu()
 
 def add_task():
 	task = input("Enter task: ")
 	tasks.append(task)
 	print("Tasks added")
+
+def do_nothing():
+	pass
+
+menu()
